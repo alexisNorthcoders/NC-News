a9ffc967edca96e2fc38d8b2c6fae029e711a3ab
alexisNorthCoders <alexiscreoulo@hotmail.com>
2024-02-06 11:38:24 +0000

    add Toggle Comments button to Article page

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -9,14 +9,23 @@ import { useParams, useNavigate } from "react-router-dom";
 export default function Article() {
   const [article, setArticle] = useState({});
   const { article_id } = useParams();
+  const [comments, setComments] = useState([]);
+  const [showComment, setShowComment] = useState(false);
+
   const { navigation, setNavigation } = useContext(NavContext);
   const navigate = useNavigate();
+  function handleShowCommentsClick() {
+    setShowComment((currentShow) => !currentShow);
+  }
   useEffect(() => {
     setNavigation((current) => {
       return { ...current, header: "article" };
     });
     fetchArticleById(article_id).then((article) => {
       setArticle(article);
+      fetchCommentsByArticleId(article.article_id).then((comments) => {
+        setComments(comments);
+      });
     });
   }, []);
   return (
@@ -49,16 +58,39 @@ export default function Article() {
       </Navbar>
       <Container>
         <Card className="g-3 pt-2 mt-5 article">
+          <Card.Title>{article.title}</Card.Title>
           <Card.Img variant="top" src={article.article_img_url} />
           <Card.Body>
-            <Card.Title>{article.title}</Card.Title>
-            <Card.Text>{article.body}</Card.Text>
-            <Card.Footer>
-              {article.votes} | Comments | {article.author} |{" "}
-              {timeDifference(article.created_at)}
+          <Card.Header className="fs-5 ">{article.author}</Card.Header>
+            <Card.Text className="fs-4">{article.body}</Card.Text>
+            <Card.Footer className="fs-4 d-flex justify-content-between">
+             <span> Votes: {article.votes}</span>
+              <Button className="fs-4"variant="light" onClick={handleShowCommentsClick}>
+                {comments.length} Comments
+              </Button>
+            <span>{timeDifference(article.created_at)}</span>
             </Card.Footer>
           </Card.Body>
         </Card>
+        {showComment ? (
+          <Col>
+            {comments.map((comment, index) => {
+              return (
+                <Card
+                  className="comments" key={`${index}+${comment.created_at}`}
+                >
+                  <Card.Title key={`${index}+${comment.created_at}`}>
+                    {comment.author}
+                  </Card.Title>
+                  <Card.Text>{comment.body}</Card.Text>
+                  <Card.Footer > 
+                    {comment.votes} | {timeDifference(comment.created_at)}
+                  </Card.Footer>
+                </Card>
+              );
+            })}
+          </Col>
+        ) : null}
       </Container>
     </>
   );
